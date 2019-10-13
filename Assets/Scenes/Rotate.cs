0130a0fd7d877e5b8d665a0ba6221bdf8e579c42
AlbaFontaneda <alba.fontaneda@gmail.com>
2019-10-13 17:26:11 +0200

    Testing with scripts

--- Assets/Scenes/Rotate.cs
+++ Assets/Scenes/Rotate.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+
+    public Transform m_AnotherCube;
+    public Vector3 m_AxisVelocity;
+    
+    [SerializeField]
+    private Vector3 m_AnotherRotationSpd;
+
+    [System.Serializable]
+    public class SerializableDataStruct
+    {
+        public int m_Indice;
+        public string m_Valor;
+    }
+    public SerializableDataStruct m_SerializedStruct;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,10 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(3.4f, 0f, 0f);
+        //transform.Rotate(3.4f, 0f, 0f);
+
+        //GameObject go = GameObject.Find("AnotherObject");
+        //go.transform.Rotate(0f, 0f, 5f);
+        m_AnotherCube.transform.Rotate(m_AxisVelocity);
     }
 }
